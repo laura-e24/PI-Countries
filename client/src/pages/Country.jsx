3357ee61851c93bd58723a58b2b14b01e7afc94d
laura-e24 <72033353+laura-e24@users.noreply.github.com>
2022-11-21 23:22:57 -0300

    fetcheo works

--- client/src/pages/Country.jsx
+++ client/src/pages/Country.jsx
@@ -1,6 +1,5 @@
 import { useParams } from 'react-router-dom'
 
-
 const Country = () => {
 
   const params = useParams();
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,15 +1,25 @@
-import { useSearchParams  } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+import { Outlet, useSearchParams  } from 'react-router-dom'
+import axios from "axios"
 
 const Home = () => {
-
   const [searchParams] = useSearchParams();
   const name = searchParams.get('name');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`http://localhost:3001/countries?name=${name}`)
+      console.log(response)
+    }
+    fetchData()
+  }, [])
 
   return (  
     <main>
       <h1>Henry Countries --- HOME --- {
         name && <span>{name}</span>
       }</h1>
+      <Outlet />
     </main>
   );
 }
