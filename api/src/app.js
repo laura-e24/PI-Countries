4900c9a913e737ac398164bec0ab67b254092f6c
laura-e24 <72033353+laura-e24@users.noreply.github.com>
2022-11-30 20:55:44 -0300

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 const countryRoutes = require('./routes/countryRoutes.js');
 const activityRoutes = require('./routes/activityRoutes.js');
 
@@ -10,14 +11,14 @@ require('./db.js');
 const server = express();
 
 server.name = 'API';
-
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Origin', 'https://henrycountries-laura-e24.up.railway.app/*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
