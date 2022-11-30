1474bb6ad0c76efe192e8f8d4106dda194fc0d84
laura-e24 <72033353+laura-e24@users.noreply.github.com>
2022-11-30 20:36:51 -0300

    remove dotevn

--- client/src/index.js
+++ client/src/index.js
@@ -6,10 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux";
 import store from "./redux/store/index.js";
-import dotenv from 'dotenv'
 import axios from 'axios';
 
-// dotenv.config()
 axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001'
 
 ReactDOM.render(
