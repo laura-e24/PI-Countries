1122659d6bada0af31a28c32e9865648c8997142
laura-e24 <72033353+laura-e24@users.noreply.github.com>
2022-11-30 20:30:26 -0300

    remove dotevn

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,7 @@ import store from "./redux/store/index.js";
 import dotenv from 'dotenv'
 import axios from 'axios';
 
-dotenv.config()
+// dotenv.config()
 axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001'
 
 ReactDOM.render(
