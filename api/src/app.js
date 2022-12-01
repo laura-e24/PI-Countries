e82356acaeb6f2a9ce61ee07c2b1e20a83c68860
laura-e24 <72033353+laura-e24@users.noreply.github.com>
2022-11-30 21:03:12 -0300

    cors

--- api/src/app.js
+++ api/src/app.js
@@ -12,9 +12,11 @@ const server = express();
 
 server.name = 'API';
 
+const whitelist = ['https://henrycountries-laura-e24.vercel.app/']
+
 const corsOptions = {
   origin: (origin, callback) => {
-    if (whitelist.includes('https://henrycountries-laura-e24.vercel.app')) {
+    if (whitelist.includes(origin)) {
       // Puede consultar la API
       callback(null, true)
     } else {
@@ -31,7 +33,7 @@ server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
   // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Origin', 'henrycountries-laura-e24.vercel.app'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://henrycountries-laura-e24.vercel.app/'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
