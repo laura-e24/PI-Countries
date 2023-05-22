77af613f01c5bf38d7c355c7857c2a3faacc8c6a
Laura Eliceiry <72033353+laura-e24@users.noreply.github.com>
2023-05-22 16:49:35 -0300

    Update ActivityForm.jsx

--- client/src/components/ActivityForm.jsx
+++ client/src/components/ActivityForm.jsx
@@ -87,7 +87,7 @@ const ActivityForm = ({ type = "create" }) => {
         countries: activity?.countries
       })
     }
-  })
+  }, [activity, type])
 
   const [errors, setErrors] = useState({
     name: "",
